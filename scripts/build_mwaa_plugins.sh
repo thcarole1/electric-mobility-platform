3f9956d55242bc095f50d80f50da9c1beabc140f
thierry <thcarole1@hotmail.com>
2026-08-31 10:43:56 +0200

    feat(validation): ajoute la validation de schema au DAG MWAA et inclut validation/ dans le build des plugins

--- scripts/build_mwaa_plugins.sh
+++ scripts/build_mwaa_plugins.sh
@@ -10,6 +10,7 @@ cp -r src/ingestion mwaa-plugins/
 cp -r src/cleaning mwaa-plugins/
 cp -r src/warehouse mwaa-plugins/
 cp -r src/simulation mwaa-plugins/
+cp -r src/validation mwaa-plugins/
 
 # Nettoyage des artefacts inutiles avant de zipper
 find mwaa-plugins -name "__pycache__" -type d -exec rm -rf {} + 2>/dev/null
